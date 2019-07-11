9cf921ca99abad9f3f59f78d76ae353b7acb3a12
Ben Davis <bdavs3000@gmail.com>
2019-07-10 17:03:47 -0700

    Home page layout + added libraries • Made a nice layout for the homepage on account of a new direction that I realized would be cool for the site • Added a CSS library called Aos for “on-scroll” animation • Added a smooth scrolling library

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import AOS from "Aos";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import Head from "../components/head";
 import Layout from "../components/layout";
@@ -6,17 +8,43 @@ import Image from "../components/image";
 
 import "../styles.scss";
 import "./styles/index.scss";
+import "aos/dist/aos.css";
+
+AOS.init();
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Head title="Home" />
 
     <section className="section">
-      <Image fileName="1920x1080.png" />
+      <section className="hero main-img">
+        <Image fileName="1920x1080.png" />
+      </section>
+      <section className="hero">
+        <div className="hero-body has-text-centered">
+          <div className="columns">
+            <div className="navigator-item column">
+              <AnchorLink href="#past" offset="25">
+                <h1 className="navigator-heading title">My Past</h1>
+              </AnchorLink>
+            </div>
+            <div className="navigator-item column">
+              <AnchorLink href="#present" offset="25">
+                <h1 className="navigator-heading title">My Present</h1>
+              </AnchorLink>
+            </div>
+            <div className="navigator-item column">
+              <AnchorLink href="#future" offset="25">
+                <h1 className="navigator-heading title">My Future</h1>
+              </AnchorLink>
+            </div>
+          </div>
+        </div>
+      </section>
     </section>
 
-    <section id="about" className="section">
-      <h1 className="title is-2 has-text-centered">About me</h1>
+    <section id="past" className="section content">
+      <h1 className="title is-2 has-text-centered">My Past</h1>
       <section className="hero">
         <div className="hero-body">
           Fusce convallis metus id felis luctus adipiscing. Etiam sit amet orci
@@ -28,44 +56,28 @@ const IndexPage = ({ data }) => (
       </section>
     </section>
 
-    <section id="skills" className="section">
-      <h1 className="title is-2 has-text-centered">Skills</h1>
-      <section className="hero">
-        <div className="hero-body">Here are my skills</div>
-      </section>
-    </section>
-
-    <section id="education" className="section">
-      <h1 className="title is-2 has-text-centered">Education</h1>
+    <section id="present" className="section content">
+      <h1 className="title is-2 has-text-centered">My Present</h1>
       <section className="hero">
         <div className="hero-body">
-          <p>This is my education</p>
+          Fusce convallis metus id felis luctus adipiscing. Etiam sit amet orci
+          eget eros faucibus tincidunt. Fusce neque. Aenean imperdiet. Cras
+          sagittis. Donec vitae sapien ut libero venenatis faucibus. Nunc nulla.
+          Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec,
+          quam. Aenean viverra rhoncus pede. Nulla sit amet est.
         </div>
       </section>
     </section>
 
-    <section id="experience" className="section">
-      <h1 className="title is-2 has-text-centered">Experience</h1>
+    <section id="future" className="section content">
+      <h1 className="title is-2 has-text-centered">My Future</h1>
       <section className="hero">
         <div className="hero-body">
-          <div className="container">
-            <h1 className="title is-4">Mobiedock</h1>
-            <h2 className="subtitle">
-              Aug. 2018 – Jun. 2019, <em>Director of Software</em>
-            </h2>
-          </div>
-          <div className="container">
-            <h1 className="title is-4">ProGuides</h1>
-            <h2 className="subtitle">
-              Aug. 2018 – Nov. 2018, <em>Content Creator</em>
-            </h2>
-          </div>
-          <div className="container">
-            <h1 className="title is-4">Stein Creative Agency</h1>
-            <h2 className="subtitle">
-              May 2018 – Oct. 2018, <em>Web Developer</em>
-            </h2>
-          </div>
+          Fusce convallis metus id felis luctus adipiscing. Etiam sit amet orci
+          eget eros faucibus tincidunt. Fusce neque. Aenean imperdiet. Cras
+          sagittis. Donec vitae sapien ut libero venenatis faucibus. Nunc nulla.
+          Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec,
+          quam. Aenean viverra rhoncus pede. Nulla sit amet est.
         </div>
       </section>
     </section>
