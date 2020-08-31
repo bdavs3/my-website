d89af1e5f723d0864a23bdfe660a2c7533e9ca31
Ben Davis <bdavs3000@gmail.com>
2020-08-31 13:36:57 -0700

    Remove dummy info from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import AOS from "Aos";
-import AnchorLink from "react-anchor-link-smooth-scroll";
 
 import Head from "../components/head";
 import Layout from "../components/layout";
-import FluidImage from "../components/utilities/fluid-image";
 
 import "../styles.scss";
 import "./styles/index.scss";
@@ -16,69 +14,6 @@ const IndexPage = () => (
   <Layout>
     <div className="index-wrapper">
       <Head title="Home" />
-      <section className="section">
-        <section className="hero main-img">
-          <FluidImage fileName="1920x1080.png" />
-        </section>
-        <section className="hero">
-          <div className="navigator hero-body has-text-centered">
-            <div className="columns">
-              <div className="navigator-item column">
-                <AnchorLink href="#past" offset="25">
-                  <h1 className="navigator-heading title">My Past</h1>
-                </AnchorLink>
-              </div>
-              <div className="navigator-item column">
-                <AnchorLink href="#present" offset="25">
-                  <h1 className="navigator-heading title">My Present</h1>
-                </AnchorLink>
-              </div>
-              <div className="navigator-item column">
-                <AnchorLink href="#future" offset="25">
-                  <h1 className="navigator-heading title">My Future</h1>
-                </AnchorLink>
-              </div>
-            </div>
-          </div>
-        </section>
-      </section>
-      <section id="past" className="section content">
-        <h1 className="is-1 title has-text-centered">My Past</h1>
-        <section className="hero">
-          <div className="hero-body">
-            Fusce convallis metus id felis luctus adipiscing. Etiam sit amet
-            orci eget eros faucibus tincidunt. Fusce neque. Aenean imperdiet.
-            Cras sagittis. Donec vitae sapien ut libero venenatis faucibus. Nunc
-            nulla. Pellentesque libero tortor, tincidunt et, tincidunt eget,
-            semper nec, quam. Aenean viverra rhoncus pede. Nulla sit amet est.
-          </div>
-        </section>
-      </section>
-      <section id="present" className="section content">
-        <h1 className="title is-1 has-text-centered">My Present</h1>
-        <section className="hero">
-          <div className="hero-body">
-            Fusce convallis metus id felis luctus adipiscing. Etiam sit amet
-            orci eget eros faucibus tincidunt. Fusce neque. Aenean imperdiet.
-            Cras sagittis. Donec vitae sapien ut libero venenatis faucibus. Nunc
-            nulla. Pellentesque libero tortor, tincidunt et, tincidunt eget,
-            semper nec, quam. Aenean viverra rhoncus pede. Nulla sit amet est.
-          </div>
-        </section>
-      </section>
-      <section id="future" className="section content">
-        <h1 className="title is-1 has-text-centered">My Future</h1>
-        <section className="hero">
-          <div className="hero-body">
-            Fusce convallis metus id felis luctus adipiscing. Etiam sit amet
-            orci eget eros faucibus tincidunt. Fusce neque. Aenean imperdiet.
-            Cras sagittis. Donec vitae sapien ut libero venenatis faucibus. Nunc
-            nulla. Pellentesque libero tortor, tincidunt et, tincidunt eget,
-            semper nec, quam. Aenean viverra rhoncus pede. Nulla sit amet est.
-          </div>
-        </section>
-      </section>
-      '
     </div>
   </Layout>
 );
