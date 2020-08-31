e6b49f51c08dd745771d6fe6fcd083bcd3055f71
Ben Davis <bdavs3000@gmail.com>
2020-08-31 16:20:34 -0700

    Add profile photo to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import AOS from "Aos";
+import { IconContext } from "react-icons";
+import { BsCodeSlash, BsMusicNoteBeamed } from "react-icons/bs";
+import { BiRun } from "react-icons/bi";
 
 import Head from "../components/head";
 import Layout from "../components/layout";
@@ -7,6 +10,7 @@ import Layout from "../components/layout";
 import "../styles.scss";
 import "./styles/index.scss";
 import "aos/dist/aos.css";
+import FluidImage from "../components/utilities/fluid-image";
 
 AOS.init();
 
@@ -16,22 +20,54 @@ const IndexPage = () => (
       <Head title="Home" />
       <section className="section index-section">
         <div className="columns">
-          <div className="column">One</div>
+          <div className="column">
+            <div>
+              <FluidImage fileName="headshot.jpg" />
+            </div>
+          </div>
           <div className="column">
             <p>
-              Hi, I'm Ben, a software developer originally from Phoenix, AZ. I
-              have been living in Los Angeles since I started at Loyola
-              Marymount University in the Fall of 2015. After graduating, I
-              moved to the sleepy beach town of El Segundo. <br /> <br />
-              This website was a fun project that now serves as a way to keep
-              track of my thoughts from time to time. If you'd like to get in
-              touch, your best bet is to reach out to my email, which can be
-              found in the footer.
+              Hi! I'm <span className="name">Ben</span>, a software developer
+              originally from Phoenix, AZ. I have been living in Los Angeles
+              since I started at Loyola Marymount University in the Fall of
+              2015. After graduating, I moved to the sleepy beach town of El
+              Segundo. <br /> <br />
+              This website serves as a way to keep track of my thoughts from
+              time to time. If you'd like to get in touch, your best bet is to
+              reach out to my email, which can be found in the footer.
             </p>
           </div>
         </div>
       </section>
-      <section className="section index-section">Three</section>
+      <section className="section index-section" style={{ paddingTop: "25px" }}>
+        <h1 className="is-centered">Some of my favorite things...</h1>
+        <div className="columns" style={{ paddingTop: "25px" }}>
+          <div className="column">
+            <h2>Coding</h2>
+            <IconContext.Provider
+              value={{ className: "react-icons center-horizontal" }}
+            >
+              <BsCodeSlash />
+            </IconContext.Provider>
+          </div>
+          <div className="column">
+            <h2>Running</h2>
+            <IconContext.Provider
+              value={{ className: "react-icons center-horizontal" }}
+            >
+              <BiRun />
+            </IconContext.Provider>
+          </div>
+          <div className="column">
+            <h2>Music Production</h2>
+            <IconContext.Provider
+              value={{ className: "react-icons center-horizontal" }}
+            >
+              <BsMusicNoteBeamed />
+            </IconContext.Provider>
+          </div>
+        </div>
+      </section>
     </div>
   </Layout>
 );
